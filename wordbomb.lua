6eababe4c73e9133e1a3a725fb3eea81d66a9658
sigmacodeslol <m96153045@gmail.com>
2025-08-23 20:01:51 +0100

    Update wordbomb.lua

--- wordbomb.lua
+++ wordbomb.lua
@@ -1,2 +1,2 @@
-getgenv().short = true  -- make this false if you want to get long words (ps you must have a rather high wpm like atleast 90)
+_G.short = true  -- make this false if you want to get long words (ps you must have a rather high wpm like atleast 90)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sigmacodeslol/neounpatchabomb/refs/heads/master/wordbomb.lua"))()
