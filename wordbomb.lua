8d9c4999736d5f4985ead2816aaa3cea3dc5afad
signumt <tcrrxzz@gmail.com>
2025-10-19 18:31:22 +0100

    Update URL for wordbomb.lua script source

--- wordbomb.lua
+++ wordbomb.lua
@@ -1,3 +1,3 @@
 _G.short = true  -- make this false if you want to get long words (ps you must have a rather high wpm like atleast 90)
 -- _G.short is always set to true when it is found as nil but the second script execution set to false or the negated bool value of the previous script execution
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sigmacodeslol/neounpatchabomb/refs/heads/master/wordbomb.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thesignumt/neounpatchabomb/refs/heads/master/wordbomb.lua"))()
