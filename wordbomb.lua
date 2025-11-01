af774b258d81b7f51bf6bec749ea26ac85955216
signumt <tcrrxzz@gmail.com>
2025-11-01 21:58:13 +0000

    Refactor wordbomb script loading and add comments
    Updated the loading mechanism for the wordbomb script and added comments for dictionary prioritization.

--- wordbomb.lua
+++ wordbomb.lua
@@ -1,3 +1,5 @@
-_G.short = true  -- make this false if you want to get long words (ps you must have a rather high wpm like atleast 90)
--- _G.short is always set to true when it is found as nil but the second script execution set to false or the negated bool value of the previous script execution
-loadstring(game:HttpGet("https://raw.githubusercontent.com/thesignumt/neounpatchabomb/refs/heads/master/wordbomb.lua"))()
+
+wb = loadstring(game:HttpGet("https://raw.githubusercontent.com/thesignumt/neounpatchabomb/refs/heads/master/wordbomb.lua"))()
+-- right now the dict is small.
+-- to prioritise getting other type of words:
+-- wb.set_dict( and then put "small", "long" or "hyphen" (hyphen is hyphenated words)
