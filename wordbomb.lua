275eb1caa1a87b421e091e462d641896f376619a
signumt <tcrrxzz@gmail.com>
2025-11-09 21:20:58 +0000

    Update wordbomb.lua

--- wordbomb.lua
+++ wordbomb.lua
@@ -1,5 +1,5 @@
-
 local wb = loadstring(game:HttpGet("https://raw.githubusercontent.com/thesignumt/neounpatchabomb/refs/heads/master/wordbomb.lua"))()
--- right now the dict is small.
--- to prioritise getting other type of words:
--- wb.set_dict( and then put "small", "long" or "hyphen" (hyphen is hyphenated words)
+wb("small") -- replace "small" with either one of these:
+-- "small"
+-- "long"
+-- "hyphen"
